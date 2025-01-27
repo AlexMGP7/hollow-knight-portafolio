9df9b588e3514198426a91a46b24d3e34ce9d1fb
Alexander <agonzalezpastena7t@gmail.com>
2025-01-27 00:29:11 -0400

    hornet base

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './global.css'
-import { Header } from './components'
+import './global.css';
+import { Header } from './components';
+import Hornet from './components/Hornet';
 
 function App() {
   return (
-    <>
-      <main>
-        <Header />
-      </main>
-    </>
-  )
+    <main className="relative w-full min-h-screen bg-gray-900 text-white">
+      <Header />
+      <Hornet speed={120} />
+    </main>
+  );
 }
 
-export default App
+export default App;
